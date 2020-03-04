1323833f7799c40485e525615ec36cc9a2809bae
Damian Wildie <damian@wildie.com>
2020-03-04 20:55:35 +1000

    Corrections

--- cpmfs/cmds/copyAll.sh
+++ cpmfs/cmds/copyAll.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 usage() {
-  echo "Usage: $0 <image_file> <target_disk> <disks> <source_dir>"
+  echo "Usage: $0 <image_file> <target_disk> <disks> <source_dir> <samples_disk> <samples_dir>"
 }
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 6 ]; then
   usage
   exit 1
 fi
@@ -13,6 +13,8 @@ IMAGE=$1
 TARGET=$2
 DISKS=$3
 SRC=$4
+SAMPLES_DISK=$5
+SAMPLES_DIR=$6
 
 FMT=4mb-hd
 
@@ -22,11 +24,14 @@ do
   DISK=$SRC/DISK$(( $n + 1))
 
   if [[ -d "$DISK" ]]; then
-    echo "Copying $DISK to partition $n"
+    echo "Copying $DISK to partition $TARGET"
     cpmcp -f ${FMT}-$TARGET $IMAGE $DISK/* 0:
   fi
 
   n=$(( $n + 1))
 done
+
+echo "Copying samples to partition $SAMPLES_DISK"
+cpmcp -f ${FMT}-$SAMPLES_DISK $IMAGE $SAMPLES_DIR/* 0:
 
 
