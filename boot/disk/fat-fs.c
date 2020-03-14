da93603fcd60a94ed88c7fa5d55fdfcda9fc6b50
Damian Wildie <damian@wildie.com>
2020-03-14 10:38:51 +1000

    prepend filename with a slash if it is not present

--- boot/disk/fat-fs.c
+++ boot/disk/fat-fs.c
@@ -17,3 +17,30 @@ void listFatDirectory(const char *path)
 //        }
 //	}
 }
+
+static FL_FILE* file = NULL;
+
+int fOpenFAT(const char *path)
+{
+	char name[255];
+	if (path[0] == '/') {
+		strcpy(name, path);
+	} else {
+		name[0] = '/';
+		strcpy(&name[1], path);
+	}
+
+	file = fl_fopen(name, "r");
+	return file == NULL ? 1 : 0;
+}
+
+int fReadFAT(int count, void *buffer)
+{
+	return fl_fread(buffer, 1, count, file);
+}
+
+void fCloseFAT()
+{
+	fl_fclose(file);
+	file = NULL;
+}
